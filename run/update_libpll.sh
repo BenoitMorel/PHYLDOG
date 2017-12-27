e86d1acdd71a46716ba8263a64d12e19ec452ad9
BenoitMorel <benomorel@gmail.com>
2017-12-27 10:57:47 +0100

    small fixes in scripts

--- run/update_libpll.sh
+++ run/update_libpll.sh
@@ -3,6 +3,6 @@ phyldogpath=$('pwd')/..
 cd /home/morelbt/github/raxml-ng/libs/pll-modules/libs/libpll
 make
 cp src/.libs/*.so* ${phyldogpath}/dependencies/lib/
-cp src/pll.h $(phyldogpath)/dependencies/include/pll
+cp src/pll.h $(phyldogpath)/dependencies/include/pllmodules
 
 
